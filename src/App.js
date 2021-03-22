c62c8495298bce2bc39f9c8da2a1fd5a0b6db050
Carlos Jaime De Leon <cjcdeleon@gmail.com>
2021-03-22 22:14:57 +1100

    add bootstrap using 'npm install react-bootstrap bootstrap'
    - https://create-react-app.dev/docs/adding-bootstrap/
    - https://react-bootstrap.github.io/getting-started/introduction/

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import { Container, Jumbotron, ToastHeader } from 'react-bootstrap';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <Container>
+    <Jumbotron>
+      <h1 class="text-center">Carlos Jaime De Leon</h1>
+    </Jumbotron>
+  </Container>
+);
 
 export default App;
